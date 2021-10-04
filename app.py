6e4a77359395e3b335e2959dd1cbf33578d5134e
Edison Garcia <egachi@live.com>
2021-10-04 11:00:31 -0500

    Bind all ip addresses

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def home():
     return f"Length of processed data: { len(processed_data)}"
 
 if __name__ == '__main__':
-    app.run()
-
+    port= os.environ.get('PORT')
+    app.run(host='0.0.0.0', debug=False, port=port)
