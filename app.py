e589ac180793c8e40b10804e16ad8617da5d8f50
Edison Garcia <egachi@live.com>
2021-10-04 12:07:48 -0500

    Adding memory method

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.signal
 from flask import Flask
 import os
+from random import random
 
 app = Flask(__name__)
 
@@ -16,11 +17,27 @@ def process_data(data):
     detrended = scipy.signal.detrend(data, axis=0)
     return detrended
 
+def generate_data(id):
+    data = []
+    i=0
+    for _ in range(id):
+	    value = random()
+	    data.append(value)
+    return data
+
+
 @app.route("/")
 def home():
     raw_data = create_data()
     processed_data = process_data(raw_data)
     return f"Length of processed data: { len(processed_data)}"
+
+array = []
+@app.route("/memory")
+def memory():
+    random = 200000 * 1024
+    array.append(generate_data(random))
+    return f"Length of processed data: { len(array)}"
 
 if __name__ == '__main__':
     port= os.environ.get('PORT')
